0692473647e2970551f4f88b407ab0367e072dcc
Zaki Zemmour <zzr@edifixio.com>
2018-08-22 15:49:35 +0200

    Ajout des fichiers BATCH

--- Fichiers batch/LancementServerFlask.bat
+++ Fichiers batch/LancementServerFlask.bat
@@ -1,5 +1,6 @@
 :: Remplacer le répertoire : C:/Users/zzr/flask_projets/git_repo/API par le répertoire contenant l'API dans votre machine
 
+
 CALL C:/Users/zzr/flask_projets/git_repo/API/venv\Scripts\activate.bat
 cd C:/Users/zzr/flask_projets/git_repo/API/venv/app
 set FLASK_APP=app.py
